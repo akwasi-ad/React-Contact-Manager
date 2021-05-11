b25299c3d6a095b0ae401b85c766cf5fdd015209
akwasi-ad <akwasicaleb111@gmail.com>
2021-05-11 21:36:32 +0000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Contacts from "./Components/Contacts";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>THE APP COMPONENT</h1>
+     <Contacts />
+     <Contacts />
+     <Contacts />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Components/Contacts.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+ class Contacts extends Component {
+    render() {
+        return (
+            <div>
+                <h4>John Doe</h4>
+                <ul>
+                    <li>Email:KwasiKont@gmail.com</li>
+                    <li>Phone:020-529-4019</li>
+                </ul>
+                
+            </div>
+        )
+    }
+}
+export default Contacts;
+
+
+
